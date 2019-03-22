685ec67868fbe12af11a22e6a2cf8abff3c125c9
ted <ted10401@gmail.com>
2019-03-23 00:51:12 +0800

    Add the shader.

---
+++ Assets/RawImageScrolling.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageScrolling : MonoBehaviour
+{
+    [SerializeField] private Vector2 m_scrollSpeed;
+    private RawImage m_rawImage;
+    private Rect m_uvRect;
+
+    private void Awake()
+    {
+        m_rawImage = GetComponent<RawImage>();
+        m_uvRect = m_rawImage.uvRect;
+    }
+
+    private void Update()
+    {
+        m_uvRect.x -= m_scrollSpeed.x * Time.deltaTime;
+        m_uvRect.y -= m_scrollSpeed.y * Time.deltaTime;
+        m_rawImage.uvRect = m_uvRect;
+    }
+}
